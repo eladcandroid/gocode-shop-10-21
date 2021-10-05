996a2409bc14b4f534da213ce2957671fb0a96fe
Elad Cohen <elad@codicate.com>
2021-10-05 21:44:02 +0300

    first commit

---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,12 @@
+import "./Todos.css";
+
+function Todos() {
+  return (
+    <div>
+      Todos 12323
+      <input className="Todos-input" type="text" />
+    </div>
+  );
+}
+
+export default Todos;
